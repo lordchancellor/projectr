53fd0469b45d85475a83d57ce4c8bb1345ce2f40
lordchancellor <c.d.j.ford@gmail.com>
2018-07-10 22:40:45 +0100

    Prepared MaterialModule for AngularMaterial imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-		AppRoutingModule
+		AppRoutingModule,
+		MaterialModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
